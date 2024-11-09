b3fedbf9cdf698a2ce9be3841764ed98018abf3d
Avouchment <156472299+avouchment@users.noreply.github.com>
2024-11-09 17:47:23 -0500

    modifying app with new fetch request to get user task list for the day

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+  const [user, setUser] = useState({ username: 'Fishy', user_id: 1 });
+  const [todaysList, setList] = useState([
+    { task: 'iron clothes', task_id: 1 },
+    { task: 'go for a run', task_id: 2 },
+    { task: 'go grocery shopping', task_id: 3 },
+  ]);
+  const [activeTab, setActiveTab] = useState('login');
+
+  useEffect(() => {
+    //get user data
+    fetch(`http://localhost:3000/dayview/user?id=${user_id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.tasklist) setActiveTab('dayview');
+        if (data.tasklist) set;
+      });
+  }, []);
+
+  return <div className='App'></div>;
+}
+
+export default App;
+
+/* react app boilerplate: <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +39,4 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+      </header> */
