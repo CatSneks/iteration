daf2f30ff1f10833dd973f429e9a9f1089485d7e
Garrett <garrettlchow@gmail.com>
2024-11-13 17:38:05 -0800

    styled the header on dashboard page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-blue': '#5658CC',
+      },
+    },
   },
   plugins: [],
 };
